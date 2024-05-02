6f9d8c36f957b3e5da039ce7f8f5677480e9d02b
Arimurilloh <161553052+Arimurilloh@users.noreply.github.com>
2024-05-02 03:27:36 +0000

    Creacion de Generador de Dominios, se utiliza un for anidado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let extensions = [".com", ".net", ".us"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let domains = [];
+
+  for (let a = 0; a < pronoun.length; a++) {
+    for (let b = 0; b < adj.length; b++) {
+      for (let c = 0; c < noun.length; c++) {
+        let link = pronoun[a] + adj[b] + noun[c];
+        for (let d = 0; d < extensions.length; d++) {
+          domains.push(link + extensions[d]);
+        }
+      }
+    }
+  }
+
+console.log(domains);
